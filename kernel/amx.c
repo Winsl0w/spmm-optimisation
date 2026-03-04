a1f36a57fd847cf5ac3a3824e5b8a8179c89fb17
Winsl0w <adamharvey@zoho.com>
2026-03-04 23:05:29 +0000

    feat: add structs for csr and bcsr formats Issue: #1 #3

--- kernel/amx.c
+++ kernel/amx.c
@@ -37,6 +37,35 @@ typedef struct __tile_config {
 
 
 
+// =========================== Sparse format definitions =====================================
+
+/*
+    CSR (Compressed Sparse Row) format for bf16 matrices
+*/
+typedef struct {
+    int nrows;      // number of rows in the matrix
+    int ncols_tiles;    // number of column tiles (number of 64 column blocks)
+    int *rowptr;    // size nrows + 1
+    int *colidx;    // size = number of nonzeros
+    uint16_t *values;   // size = number of nonzeros, bf16 values
+} csr_matrix_bf16_t;
+
+
+/*
+    BCSR (Blocked Compressed Sparse Row) format for bf16 matrices
+*/
+typedef struct {
+    int nblockrows;      // number of rows in the matrix
+    int nblockcols_tiles;    // number of column tiles (number of 64 column blocks)
+    int *browptr;    // size nrows/BLOCK_SIZE + 1
+    int *bcolidx;    // size = number of nonzero blocks
+    uint16_t *values;   // size = number of nonzero blocks * BLOCK_SIZE * 64, bf16 values
+} bcsr_matrix_bf16_t;
+
+
+
+
+
 // Initialise tile config register for bf16
 static void amx_tile_config_bf16(__tilecfg *cfg) {
     cfg->palette_id = 1;
